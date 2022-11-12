a038c323089a153c5b859484920c2f991369ee14
dansundy <dansundy@gmail.com>
2022-11-12 15:24:49 -0500

    Attempt to rescue token error

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -2,4 +2,14 @@
 
 class AuthenticatedController < ApplicationController
   include ShopifyApp::Authenticated
+
+  rescue_from StandardError,
+              ShopifyApp::ShopifyDomainNotFound,
+              with: :render_custom_error_response
+
+  private
+
+  def render_custom_error_response
+    render json: { error: 'Custom error' }, status: 400
+  end
 end
